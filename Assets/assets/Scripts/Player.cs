15bc25c7b84c177f6ba8357bc01f6d62feffe078
Oskar Walther <oskarwalther.sueptitz@gmail.com>
2025-06-10 12:38:00 +0200

    Save

---
+++ Assets/assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    public Camera playerCamera;
+    public GameObject playerBody;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+    public float intialBoostStrength = 3f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+    private CharacterController characterController;
+
+    private bool locked = true;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+
+        Vector3 Instance = playerBody.transform.position;
+        float height = playerBody.transform.localScale.y * 0.8f; // Camera height (head)
+        playerCamera.transform.position = new Vector3(Instance.x, Instance.y + height, Instance.z);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            intialBoostStrength += 0.2f;
+            moveDirection = -playerCamera.transform.forward * intialBoostStrength;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (locked)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+    }
+}
